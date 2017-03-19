1492727719d67589ca99bd3841c1aefdd1cd6312
Maulana Yusup Abdullah <adeneling@gmail.com>
2017-03-19 21:10:59 +0700

    Basic 1

---
+++ 1_basics/app.js
@@ -0,0 +1,11 @@
+const name = "Andrew";
+const details = { favouriteLanguage: "JavaScript", age: 33, children: 3 } 
+const errorMessage = "Something bad has occurred";
+
+console.log("Hello Maulana Yusup A");
+console.error("Ooopppss!!");
+console.dir(
+	{
+		name: "Maulana", age: 22
+	}
+);
