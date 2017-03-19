de62b45958c06b804824c137de70ece7430e3228
Maulana Yusup Abdullah <adeneling@gmail.com>
2017-03-19 21:43:01 +0700

    Require Https

---
+++ 1_basics/teamtreehouse.js
@@ -0,0 +1,14 @@
+/* Connect to API Team Treehouse */
+
+/* require https */
+const https = require('https');
+const username = "bramantyaprakosa2";
+
+function printMessage(username, badgeCount, points){
+	const message = `${username} has ${badgeCount} total badge(s) and ${points} points in javascript`;
+	console.log(message);
+}
+
+const request = https.get(`https://teamtreehouse.com/${username}.json`, response =>{
+	console.dir(response.statusCode);
+});
